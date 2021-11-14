a1037198f3155c0729ca2c2b4363b06e75668c6c
weeraphon <60165647+62130500131@users.noreply.github.com>
2021-11-14 15:21:41 +0700

    add edit user by id

--- src/main/java/sit/integrated/project/controllers/UsersController.java
+++ src/main/java/sit/integrated/project/controllers/UsersController.java
@@ -2,7 +2,10 @@ package sit.integrated.project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import sit.integrated.project.exceptions.ExceptionResponse;
+import sit.integrated.project.exceptions.ProductsException;
 import sit.integrated.project.models.Feedback;
+import sit.integrated.project.models.Products;
 import sit.integrated.project.models.Users;
 import sit.integrated.project.repositories.UsersRepositories;
 import java.util.List;
@@ -30,10 +33,31 @@ public class UsersController {
         usersRepositories.save(user);
         return user;
     }
+
+    @PutMapping("/Edit/{id}")
+    public Users editUser(@RequestBody Users users, @PathVariable int id){
+        if (hasFoundId(id) && (id == users.getUserId())) {
+            usersRepositories.save(users);
+            return users;
+        }
+        else throw  new ProductsException(ExceptionResponse.ERROR_CODE.ITEM_DOES_NOT_EXIST , "DOES NOT EXIST");
+
+    }
+
     @DeleteMapping("/Delete/{id}")
     public String deleteUsers(@PathVariable int id){
         usersRepositories.deleteById(id);
         String s = String.valueOf(id);
         return s+"has been deleted";
+    }
+
+    public boolean hasFoundId(int id){
+        List<Users> users = usersRepositories.findAll();
+        for (int i = 0; i < users.size(); i++) {
+            if(users.get(i).getUserId() == id){
+                return true;
+            }
+        }
+        return false;
     }
 }

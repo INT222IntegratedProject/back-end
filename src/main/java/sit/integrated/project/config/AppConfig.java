2826131392dc408d7d3ce3967c41b6935ddd11ce
weeraphon <60165647+62130500131@users.noreply.github.com>
2021-11-25 23:44:28 +0700

    add origin

--- src/main/java/sit/integrated/project/config/AppConfig.java
+++ src/main/java/sit/integrated/project/config/AppConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class AppConfig implements WebMvcConfigurer {
  @Override
     public  void  addCorsMappings( CorsRegistry registry){
-     registry.addMapping("/**")
+     registry.addMapping("/**").allowedOrigins("*")
              .allowedMethods("GET" , "POST" ,"PUT" , "DELETE");
  }
 }

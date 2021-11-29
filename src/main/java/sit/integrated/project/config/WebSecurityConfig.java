9db29dc6cb2ec5072cdde94fc6df49e4be3c8718
weeraphon <60165647+62130500131@users.noreply.github.com>
2021-11-29 23:39:10 +0700

    httpSecurity.cors!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/main/java/sit/integrated/project/config/WebSecurityConfig.java
+++ src/main/java/sit/integrated/project/config/WebSecurityConfig.java
@@ -50,6 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable();
+        httpSecurity.cors();
         httpSecurity.authorizeRequests().antMatchers("/authenticate").permitAll();
         httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         httpSecurity.authorizeRequests().antMatchers(HttpMethod.GET,"/Products/**").permitAll();

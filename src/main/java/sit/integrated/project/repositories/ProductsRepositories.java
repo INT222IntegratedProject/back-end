e380a9586a89b4d1e56043f14d4aaf3afc96e180
weeraphon <60165647+62130500131@users.noreply.github.com>
2021-11-30 23:34:22 +0700

    add findAllByUserId

--- src/main/java/sit/integrated/project/repositories/ProductsRepositories.java
+++ src/main/java/sit/integrated/project/repositories/ProductsRepositories.java
@@ -3,8 +3,14 @@ package sit.integrated.project.repositories;
 import org.springframework.data.jpa.repository.Query;
 import sit.integrated.project.models.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
+import sit.integrated.project.models.Users;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductsRepositories extends JpaRepository<Products,Integer> {
     @Query(value = "SELECT max(productId) from Products ")
-    public int productLatestId();
+     int productLatestId();
+
+     List<Products> findAllByUserId (Optional<Users> userId);
 }

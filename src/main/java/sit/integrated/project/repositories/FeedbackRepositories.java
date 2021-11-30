8e508f3617fba779d4e6f5ff14b3b3565b3bd749
weeraphon <60165647+62130500131@users.noreply.github.com>
2021-11-30 23:50:49 +0700

    add findAllByproductsId

--- src/main/java/sit/integrated/project/repositories/FeedbackRepositories.java
+++ src/main/java/sit/integrated/project/repositories/FeedbackRepositories.java
@@ -3,7 +3,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import sit.integrated.project.models.Feedback;
 
+import java.util.List;
+
 public interface FeedbackRepositories extends JpaRepository<Feedback, Integer> {
     @Query(value = "SELECT max(feedbackId) from Feedback ")
-    public int feedbackLatestId();
+     int feedbackLatestId();
+
+    List<Feedback> findAllByproductsId(Integer productsId);
 }

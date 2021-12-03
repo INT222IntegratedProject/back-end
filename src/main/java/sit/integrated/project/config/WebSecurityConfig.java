3b98e0ffb998fc3668d1da02988d7863bbb5375a
weeraphon <60165647+62130500131@users.noreply.github.com>
2021-12-03 19:29:14 +0700

    update permission

--- src/main/java/sit/integrated/project/config/WebSecurityConfig.java
+++ src/main/java/sit/integrated/project/config/WebSecurityConfig.java
@@ -53,13 +53,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity.cors();
         httpSecurity.authorizeRequests().antMatchers("/authenticate").permitAll();
         httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        httpSecurity.authorizeRequests().antMatchers(HttpMethod.GET,"/Products/**").permitAll();
-        httpSecurity.authorizeRequests().antMatchers(HttpMethod.GET,"/Feedback/**").permitAll();
-        httpSecurity.authorizeRequests().antMatchers(HttpMethod.GET,"/Images/**").permitAll();
-        httpSecurity.authorizeRequests().antMatchers(HttpMethod.GET,"/Colors/**").permitAll();
-        httpSecurity.authorizeRequests().antMatchers(HttpMethod.GET,"/Brands/**").permitAll();
-        httpSecurity.authorizeRequests().antMatchers(HttpMethod.POST,"/Users/**").permitAll();
-
         
         httpSecurity.authorizeRequests().anyRequest().authenticated();
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);

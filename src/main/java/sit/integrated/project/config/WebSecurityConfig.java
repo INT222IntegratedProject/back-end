278e8f685f0089e40076d1ae6c4923ee0bb91b7b
weeraphon <60165647+62130500131@users.noreply.github.com>
2021-12-08 21:28:22 +0700

    Update WebSecurityConfig.java

--- src/main/java/sit/integrated/project/config/WebSecurityConfig.java
+++ src/main/java/sit/integrated/project/config/WebSecurityConfig.java
@@ -55,6 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity.authorizeRequests().antMatchers("/authenticate").permitAll();
         httpSecurity.authorizeRequests().antMatchers(HttpMethod.GET,"/Feedback/**").permitAll();
         httpSecurity.authorizeRequests().antMatchers(HttpMethod.GET,"/Images/**").permitAll();
+        httpSecurity.authorizeRequests().antMatchers(HttpMethod.POST,"/Users/**").permitAll();
         httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         
         httpSecurity.authorizeRequests().anyRequest().authenticated();
